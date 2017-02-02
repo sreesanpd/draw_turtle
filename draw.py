6af572e255997ab28c86e5d08352dace528a84dc
sreesanpd <sreesanpd@yahoo.co.in>
2017-02-02 10:46:53 +0530

    parameterised the functions for square and circle

--- draw.py
+++ draw.py
@@ -9,31 +9,30 @@ def close_canvas():
 	window = turtle.Screen()
 	window.exitonclick()
 
-def draw_square():
+def draw_square(some_turtle):
+	for i in range(1,5):
+		some_turtle.forward(100)
+		some_turtle.right(90)
+
+def draw_circle(some_turtle):
+	some_turtle.left(40)
+	some_turtle.circle(120)
+
+def draw_art():
+	draw_canvas()
 	
 	brad = turtle.Turtle()
 	brad.shape("turtle")
 	brad.color("blue")
-	brad.speed(1)
-
-	for i in range(1,5):
-		brad.forward(100)
-		brad.right(90)
-
-def draw_circle():
+	brad.speed(5)
+	draw_square(brad)	
 
 	angie = turtle.Turtle()
 	angie.shape("arrow")
-	angie.color("green")
+	angie.color("yellow")
 	angie.speed(7)
+	draw_circle(angie)
 
-	angie.left(40)
-	angie.circle(120)
-
-def draw_art():
-	draw_canvas()
-	draw_square()	
-	draw_circle()
 	close_canvas()
 
 draw_art()	
